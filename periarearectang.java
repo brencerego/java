0270a330af81ff320f04def467d94b15a9f5ccd8
brencerego <brencerego18@gmail.com>
2025-03-03 15:05:08 +0530

    periarearectang.java

---
+++ periarearectang.java
@@ -0,0 +1,17 @@
+package brence;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Compound { 
+    public static void main(String[] args) {  
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Length:");
+		    int l = sr.nextInt();
+		    System.out.println("Enter the Width:");
+			  int b = sr.nextInt();
+			  int perimeter = 2 * (l + b);
+			  int area = l * b;
+			  System.out.println("The Perimeter is "+perimeter+" and the area is "+area);
+     }
+}
+			
