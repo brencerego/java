1a8253c97470aa2b4b462decb1e9df18d90d70ee
brencerego <brencerego18@gmail.com>
2025-03-03 15:06:01 +0530

    compoundinterest.java

--- compoundinterest.java
+++ compoundinterest.java
@@ -6,13 +6,13 @@ public class Compound {
     public static void main(String[] args) {  
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Principal Amount:");
-			  int P = sr.nextInt();
-			  System.out.print("Enter Rate of Interest:");
-			  double R = sr.nextDouble();
-			  System.out.print("Enter the Time Period:");
-			  int n = sr.nextInt();			
-			  double CI = P * (Math.pow(1 + (R/100),n));
-	   		System.out.println("The Compound Interest After "+n+" years is "+CI);
-			  scanner.close();
+	int P = sr.nextInt();
+	System.out.print("Enter Rate of Interest:");
+	double R = sr.nextDouble();
+	System.out.print("Enter the Time Period:");
+	int n = sr.nextInt();			
+	double CI = P * (Math.pow(1 + (R/100),n));
+	System.out.println("The Compound Interest After "+n+" years is "+CI);
+	scanner.close();
         }
 }
