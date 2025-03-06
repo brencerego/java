4c421fd3d92bc378bf1415c0f483169c51cb198f
brencerego <brencerego18@gmail.com>
2025-03-06 20:34:49 +0400

    Add files via upload

--- temperature.java
+++ temperature.java
@@ -3,22 +3,21 @@ import java.util.Scanner;
 public class Temperature {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("What do you want to convert:\n1. Celsius into Farenheight(type 1)\n2. Fraenheight into Celcius(type 2)\nInput:");
+            System.out.println("What do you want to convert:\n1. Celsius into fahrenheit\n2. Fahrenheit into Celcius\nInput:");
             int n = sc.nextInt();
 
             if (n==1){
                 System.out.println("Enter temperature in Celsius: ");
                 double celsius = sc.nextDouble();
                 double fahrenheit = (celsius * 9 / 5) + 32;
-                System.out.println(celsius + "°C is " + fahrenheit + "°F");
+                System.out.println(celsius + "C is " + fahrenheit + "F");
             }
             else {
                 System.out.println("Enter temperature in Fahrenheit: ");
                 double fahrenheit = sc.nextDouble();
                 double celsius = (fahrenheit - 32) * 5 / 9;
-                System.out.println(fahrenheit + "°F is " + celsius + "°C");
+                System.out.println(fahrenheit + "F is " + celsius + "C");
             }
         }
     }
 }
-
