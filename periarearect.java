2285733e7afd0f59b429cad46e5983cd43925762
brencerego <brencerego18@gmail.com>
2025-03-07 21:11:17 +0400

    Add files via upload

--- periarearect.java
+++ periarearect.java
@@ -4,14 +4,14 @@ import java.lang.Math;
 
 public class Compound { 
     public static void main(String[] args) {  
-        Scanner scanner = new Scanner(System.in);
+        Scanner sr = new Scanner(System.in);
         System.out.println("Enter the Length:");
-		    int l = sr.nextInt();
-		    System.out.println("Enter the Width:");
-			  int b = sr.nextInt();
-			  int perimeter = 2 * (l + b);
-			  int area = l * b;
-			  System.out.println("The Perimeter is "+perimeter+" and the area is "+area);
+	int l = sr.nextInt();
+	System.out.println("Enter the Width:");
+	int b = sr.nextInt();
+	int perimeter = 2 * (l + b);
+	int area = l * b;
+	System.out.println("The Perimeter is "+perimeter+" and the area is "+area);
      }
 }
 			
